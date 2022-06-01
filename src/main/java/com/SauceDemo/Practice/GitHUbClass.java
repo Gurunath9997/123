b699ce29243ef14a1bd99adde430481e3f55291b
pavanpuri <99785740+pavanpuri@users.noreply.github.com>
2022-06-01 07:28:46 +0530

    commit the code at 07,.28 am

---
+++ src/main/java/com/SauceDemo/Practice/GitHUbClass.java
@@ -0,0 +1,13 @@
+package com.SauceDemo.Practice;
+
+import org.testng.annotations.Test;
+
+public class GitHUbClass 
+{
+	@Test
+	public void method1()
+	{
+		System.out.println("this is github class");
+	}
+
+}
